6b2088c8c82a55328678eae2090fe224e5844062
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-01-23 22:02:41 +0100

    add react routing

--- src/App.tsx
+++ src/App.tsx
@@ -6,32 +6,36 @@ import Col from "react-bootstrap/Col";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Row from 'react-bootstrap/Row';
-import ModelDisplay from "./ModelDisplay.tsx";
+import {BrowserRouter} from 'react-router-dom';
+import ModelDisplay from './ModelDisplay.tsx';
+
 
 function App() {
     return (
-        <Container fluid className="h-100">
-            <Row className="h-100">
-                <Col md={2}>
-                    <Nav defaultActiveKey="/" className="flex-column">
-                        <Nav.Link href="/">Home</Nav.Link>
-                    </Nav>
-                </Col>
-                <Col>
-                    <Canvas className="h-100">
-                        <ambientLight intensity={Math.PI / 2}/>
-                        <directionalLight position={[10, 0, 0]} intensity={Math.PI * 2}/>
-                        <Suspense fallback={null}>
-                            <ModelDisplay rotation={[-Math.PI / 2, 0, 0]}/>
-                        </Suspense>
-                        <OrbitControls enablePan={true}
-                                       enableZoom={true}
-                                       enableRotate={true}/>
-                        <Grid infiniteGrid/>
-                    </Canvas>
-                </Col>
-            </Row>
-        </Container>
+        <BrowserRouter>
+            <Container fluid className="h-100">
+                <Row className="h-100">
+                    <Col md={2}>
+                        <Nav defaultActiveKey="/" className="flex-column">
+                            <Nav.Link href="/">Home</Nav.Link>
+                        </Nav>
+                    </Col>
+                    <Col>
+                        <Canvas className="h-100">
+                            <ambientLight intensity={Math.PI / 2}/>
+                            <directionalLight position={[10, 0, 0]} intensity={Math.PI * 2}/>
+                            <Suspense fallback={null}>
+                                <ModelDisplay rotation={[-Math.PI / 2, 0, 0]}/>
+                            </Suspense>
+                            <OrbitControls enablePan={true}
+                                           enableZoom={true}
+                                           enableRotate={true}/>
+                            <Grid infiniteGrid/>
+                        </Canvas>
+                    </Col>
+                </Row>
+            </Container>
+        </BrowserRouter>
     )
 }
 
