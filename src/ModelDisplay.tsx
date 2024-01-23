274413a8fc7d8435f62e1db61850c13b09089c00
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-01-23 21:26:24 +0100

    ignore type error on gltf node

--- src/ModelDisplay.tsx
+++ src/ModelDisplay.tsx
@@ -1,7 +1,8 @@
 import ModelUrl from '/ryzom/characters/shapes/ca_ship.shape.gltf?url'
 import {useGLTF} from '@react-three/drei'
+import {GroupProps} from "@react-three/fiber";
 
-export default function ModelDisplay(props) {
+export default function ModelDisplay(props: GroupProps) {
     const {nodes, materials} = useGLTF(ModelUrl)
 
     return (
@@ -11,7 +12,10 @@ export default function ModelDisplay(props) {
             {Object.entries(nodes).map(([key, node]) =>
                 <mesh
                     key={key}
-                    geometry={node.geometry}
+                    geometry={
+                        /// @ts-ignore
+                        node.geometry
+                    }
                     material={materials[key] || materials[""]}
                 />
             )}
