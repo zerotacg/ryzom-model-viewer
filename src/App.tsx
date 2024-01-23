f8947181dad9e51dbf493f09f1b09b809ad555c2
Tobias Peters <tobias.peters@kreativeffekt.at>
2024-01-23 20:49:56 +0100

    add some rudimentery navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,36 @@
 import './App.css'
-import {OrbitControls} from '@react-three/drei'
+import {Grid, OrbitControls} from '@react-three/drei'
 import {Canvas} from '@react-three/fiber'
 import {Suspense} from 'react'
+import Col from "react-bootstrap/Col";
 import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Row from 'react-bootstrap/Row';
 import ModelDisplay from "./ModelDisplay.tsx";
 
 function App() {
     return (
-        <Container fluid="md" className="h-100">
-            <div className="h-100">
-                <Canvas className="h-100">
-                    <ambientLight intensity={Math.PI / 2}/>
-                    <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI}/>
-                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI}/>
-                    <Suspense fallback={null}>
-                        <ModelDisplay rotation={[-90, 0, 0]}/>
-                    </Suspense>
-                    <OrbitControls enablePan={true}
-                                   enableZoom={true}
-                                   enableRotate={true}/>
-                </Canvas>
-            </div>
+        <Container fluid className="h-100">
+            <Row className="h-100">
+                <Col md={2}>
+                    <Nav defaultActiveKey="/" className="flex-column">
+                        <Nav.Link href="/">Home</Nav.Link>
+                    </Nav>
+                </Col>
+                <Col>
+                    <Canvas className="h-100">
+                        <ambientLight intensity={Math.PI / 2}/>
+                        <directionalLight position={[10, 0, 0]} intensity={Math.PI * 2}/>
+                        <Suspense fallback={null}>
+                            <ModelDisplay rotation={[-Math.PI / 2, 0, 0]}/>
+                        </Suspense>
+                        <OrbitControls enablePan={true}
+                                       enableZoom={true}
+                                       enableRotate={true}/>
+                        <Grid infiniteGrid/>
+                    </Canvas>
+                </Col>
+            </Row>
         </Container>
     )
 }
